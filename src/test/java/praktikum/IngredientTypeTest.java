41f95ec3bde442da51f7a3fdfe06564ca835beb3
JUITsu <tryggolav@gmail.com>
2024-09-02 13:30:01 +0300

    first commit

---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,15 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class IngredientTypeTest {
+    @Test
+    public void checkSauceIngredientType() {
+        Assert.assertEquals("SAUCE", IngredientType.SAUCE.name());
+    }
+    @Test
+    public void checkFillingIngredientType() {
+        Assert.assertEquals("FILLING", IngredientType.FILLING.name());
+    }
+}
